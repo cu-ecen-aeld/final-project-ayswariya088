8fad8f40b1bb037e85df36b97e60dd1edb0983f9
ayswariya088 <ayka9480@colorado.edu>
2023-04-23 03:37:58 +0000

    Wifi changed

--- base_external/package/tmp_sensor/tmp_sensor.mk
+++ base_external/package/tmp_sensor/tmp_sensor.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-TMP_SENSOR_VERSION = 'cac0cc947c43678d6b5a09ba026b157ae4e316e5'
+TMP_SENSOR_VERSION = 'c73b4e55d2ea4906d725c2fb435e0cb1f6cb10e3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
